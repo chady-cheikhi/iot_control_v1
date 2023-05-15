de0a46d07456005290141cc1fd6c15c17008eca8
chady <ccheikhi2000@gmail.com>
2023-05-15 17:45:31 +0100

    Fixing functionalities bug: ajax=> post

--- NANOS/views.py
+++ NANOS/views.py
@@ -30,6 +30,27 @@ class Nano(View):
         nano.what = ''
         nano.custom_cmd = None
         nano.save()
+        return render(request, 'nano.html', {'functionalities': functionalities,
+                                                      'hostname': hostname,
+                                                      'where_dict': where_dict,
+                                                      'resolutions': resolutions
+                                                      })
+
+class Nano2(View):
+    def get(self, request, hostname):
+        resolutions = ['2160x1278', '1366x673', '3840x2400', '3840x2160', '2880x1800', '2560x1600', '2560x1440',
+                       '1920x1440', '1856x1392', '1792x1344', '1920x1200', '1920x1080', '1600x1200', '1680x1050',
+                       '1400x1050', '1280x1024', '1440x900', '1280x960', '1360x768', '1280x800', '1152x864',
+                       '1280x768', '1280x720', '1024x768', '800x600', '640x480']
+        functionalities = ['custom_cmd', 'reboot', 'screenshot', 'show_dashboard', 'show_video',
+                           'change_resolution', 'upgrade']
+        nano = NanoIoT.objects.all().get(hostname=hostname)
+        where_dict = {}
+        for place in NanoIoT.where_list:
+            where_dict[place] = list(NanoIoT.objects.filter(where=place).values_list('hostname', flat=True))
+        nano.what = ''
+        nano.custom_cmd = None
+        nano.save()
         return render(request, 'nano_reactive.html', {'functionalities': functionalities,
                                                       'hostname': hostname,
                                                       'where_dict': where_dict,
@@ -59,6 +80,7 @@ class Screenshot(View):
         print(image.name)
         return render(request, 'screenshot.html', {'hostname': hostname,
                                                    'image': image})
+
 
 class Landing(View):
     def get(self, request):
