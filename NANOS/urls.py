4615af4a37d50a808bf7d232143979ed845384c1
chady <ccheikhi2000@gmail.com>
2023-05-15 09:04:20 +0100

    front: on way to fix functionality

--- NANOS/urls.py
+++ NANOS/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Terminal, Index, Nano, Screenshot, Landing
+from .views import Terminal, Index, Nano, Screenshot, Landing, Nano2
 
 urlpatterns = [
     path("all-nanos", Index.as_view(), name='index'),
     path("nano/<str:hostname>/terminal", Terminal.as_view(), name='page-nano'),
     path("nano/<str:hostname>", Nano.as_view(), name='nano'),
+    path("nano2/<str:hostname>", Nano2.as_view(), name='nano2'),
     path("nano/<str:hostname>/screenshot", Screenshot.as_view(), name='screenshot'),
     path("", Landing.as_view(), name='landing')
 ]
